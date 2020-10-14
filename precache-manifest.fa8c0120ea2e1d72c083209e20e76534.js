22be351f127343f7c06027c7aa04998cfbbb3520
qiuhangwei <1239197997@qq.com>
2020-10-14 09:39:18 +0800

    init

---
+++ precache-manifest.fa8c0120ea2e1d72c083209e20e76534.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6545a7c605062eaeb5ed91dd18f32d18",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1b40f09c9e9c0dddb601",
+    "url": "./static/css/2.1ce32a85.chunk.css"
+  },
+  {
+    "revision": "f53bf9743ee794f2d297",
+    "url": "./static/css/main.8f801b67.chunk.css"
+  },
+  {
+    "revision": "1b40f09c9e9c0dddb601",
+    "url": "./static/js/2.99a0c266.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.99a0c266.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f53bf9743ee794f2d297",
+    "url": "./static/js/main.c016c652.chunk.js"
+  },
+  {
+    "revision": "225b81f8be50e173681b",
+    "url": "./static/js/runtime-main.9b6e7216.js"
+  },
+  {
+    "revision": "15cb7faa02437c2f9719351c157fe7e7",
+    "url": "./static/media/font-awesome.15cb7faa.less"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
