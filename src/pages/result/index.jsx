6180449303bd4646060cd9cd3f881c6719783789
qiuhangwei <1239197997@qq.com>
2020-10-15 17:14:23 +0800

    init

--- src/pages/result/index.jsx
+++ src/pages/result/index.jsx
@@ -5,7 +5,7 @@ import {Spin, Alert, Input} from 'antd'
 import 'font-awesome/less/font-awesome.less';
 import 'react-fontawesome';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {} from '@fortawesome/free-regular-svg-icons'
+import {faWindowClose} from '@fortawesome/free-regular-svg-icons'
 import {faFighterJet, faTrophy, faUsers} from '@fortawesome/free-solid-svg-icons'
 
 class Result extends Component {
@@ -14,11 +14,29 @@ class Result extends Component {
         player1: '',
         player2: '',
         winner: {},
-        loser: {}
+        loser: {},
+        isWrongPlayer: false
     }
 
     componentDidMount() {
-        console.log('哎', this)
+        console.log('哎', this.props.location.search.split('?')[1].split('&'))
+        let params = this.props.location.search.split('?')[1].split('&')
+        if (params.length < 2) {
+            this.setState({isWrongPlayer: true}, () => {
+                window.localStorage.removeItem('player1')
+                window.localStorage.removeItem('player2')
+                window.localStorage.removeItem('followers1')
+                window.localStorage.removeItem('followers2')
+            })
+        } else {
+            this.setState({isWrongPlayer: false})
+        }
+        let players = {}
+        params.forEach(param => {
+            console.log('普拉姆', param)
+            players[param.split('=')[0]] = param.split('=')[1]
+        })
+        console.log('普雷厄斯', players)
         let {player1} = window.localStorage
         if (this.props.history.location.state || player1) {
             let {player1, player2, followers1, followers2} = this.props.history.location.state || window.localStorage
@@ -58,59 +76,77 @@ class Result extends Component {
 
     doReset = () => {
         this.setState({player1: '', player2: '', winner: {}, loser: {}}, () => {
+            window.localStorage.removeItem('player1')
+            window.localStorage.removeItem('player2')
+            window.localStorage.removeItem('followers1')
+            window.localStorage.removeItem('followers2')
             this.props.history.push('/battle')
         })
     }
 
     render() {
-        let {winner, loser} = this.state
+        let {winner, loser,isWrongPlayer} = this.state
         console.log('winner', winner)
         console.log('loser', loser)
+        console.log('荣恩普蕾尔',isWrongPlayer)
         return (
-            <div className="result">
-                <div className={'result-players'}>
-                    <div className={'result-player'}>
-                        <div className={'result-player-title'}>
-                            Winner
+            <div>
+                {
+                    isWrongPlayer?
+                        <div className={'wrong-page'}>
+                            <div>
+                                <FontAwesomeIcon className={'wrong-page-icon'} icon={faWindowClose}/>
+                            </div>
+                            <div className={'wrong-page-content'}>
+                                参数错误，请<a onClick={this.doReset.bind(this)}>返回</a>
+                            </div>
+                        </div>:
+                        <div className="result">
+                            <div className={'result-players'}>
+                                <div className={'result-player'}>
+                                    <div className={'result-player-title'}>
+                                        Winner
+                                    </div>
+                                    <div className={'result-player-avatar'}>
+                                        {
+                                            winner &&
+                                            <img src={winner.avatar_url}/>
+                                        }
+                                    </div>
+                                    <div className={'result-player-name'}>
+                                        {
+                                            winner && winner.login
+                                        }
+                                    </div>
+                                    <div className={'result-player-score'}>
+                                        score:1
+                                    </div>
+                                </div>
+                                <div className={'result-player'}>
+                                    <div className={'result-player-title'}>
+                                        Loser
+                                    </div>
+                                    <div className={'result-player-avatar'}>
+                                        {
+                                            loser &&
+                                            <img src={loser.avatar_url}/>
+                                        }
+                                    </div>
+                                    <div className={'result-player-name'}>
+                                        {
+                                            loser && loser.login
+                                        }
+                                    </div>
+                                    <div className={'result-player-score'}>
+                                        score:0
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={'reset'} onClick={this.doReset}>
+                                Reset
+                            </div>
                         </div>
-                        <div className={'result-player-avatar'}>
-                            {
-                                winner &&
-                                <img src={winner.avatar_url}/>
-                            }
-                        </div>
-                        <div className={'result-player-name'}>
-                            {
-                                winner && winner.login
-                            }
-                        </div>
-                        <div className={'result-player-score'}>
-                            score:1
-                        </div>
-                    </div>
-                    <div className={'result-player'}>
-                        <div className={'result-player-title'}>
-                            Loser
-                        </div>
-                        <div className={'result-player-avatar'}>
-                            {
-                                loser &&
-                                <img src={loser.avatar_url}/>
-                            }
-                        </div>
-                        <div className={'result-player-name'}>
-                            {
-                                loser && loser.login
-                            }
-                        </div>
-                        <div className={'result-player-score'}>
-                            score:0
-                        </div>
-                    </div>
-                </div>
-                <div className={'reset'} onClick={this.doReset}>
-                    Reset
-                </div>
+                }
             </div>
         )
 
